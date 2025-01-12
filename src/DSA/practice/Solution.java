a37567c2265bce8c49a179571e9736957bd10418
Rakesh Ghosh <rakeshacot@gmail.com>
2025-01-12 22:23:25 +0530

    latest push

---
+++ src/DSA/practice/Solution.java
@@ -0,0 +1,15 @@
+package DSA.practice;
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int start = 0, mid = 0, high = n-1;
+
+        while (mid < high){
+            if (nums[mid]  == 0){
+
+            }
+        }
+    }
+}
